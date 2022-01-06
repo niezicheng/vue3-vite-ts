666e2f09c1ec79d5752ece870814a77eef44c386
niezicheng <2429705898@qq.com>
2022-01-06 17:52:57 +0800

    feat: add Vuex store config

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,3 +1,9 @@
 <template>
   <div>home page</div>
+  <HelloWorld msg="Hello Vue!!!"></HelloWorld>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+
+</script>
---
+++ src/store/index.ts
@@ -0,0 +1,40 @@
+import { createStore, Store, useStore as useVuexStore } from "vuex";
+import { InjectionKey } from "vue";
+
+export interface State {
+  count: number;
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+// 处理 state 类型
+export const useStore = () => {
+  return useVuexStore(key);
+}
+
+const store = createStore<State>({
+  // 数据源
+  state: {
+    count: 0
+  },
+  // 计算操作
+  getters: {},
+  // 同步操作
+  mutations: {
+    increment (state) {
+      state.count++;
+    }
+  },
+  // 异步操作
+  actions: {
+    incrementAction: ({ commit }) => {
+      setTimeout(() => {
+        commit('increment');
+      }, 2000)
+    }
+  },
+  // 模块划分
+  modules: {}
+})
+
+export default store;
