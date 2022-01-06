67d0b52d630dbd157693acb80ad428b5c81346b1
niezicheng <2429705898@qq.com>
2022-01-06 16:28:32 +0800

    feat: vite add base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  resolve: {
+    // 配置 src 目录映射
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+
+  base: "./", // 打包路径
+
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: "http://localhost:8080/api/", // 目标地址
+        changeOrigin: true, // 修改源
+        secure: false, // ssl
+        rewrite: path => path.replace('/api', '/')
+      }
+    }
+  }
+
 })
