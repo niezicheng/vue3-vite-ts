28c798e34381016549287503cd2bbef9bf284d33
niezicheng <2429705898@qq.com>
2022-01-06 17:00:45 +0800

    feat: add base router config

---
+++ src/pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>404 page</div>
+  <button @click="onBack">返回</button>
+</template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router';
+  const router = useRouter();
+
+  // 返回上页
+  const onBack = () => {
+    router.go(-1);
+  }
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import Home from '@/pages/home/index.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/:pathMath(.*)*',
+    component: () => import('@/pages/404.vue')
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+// 路由拦截器, 自定义拦截内容
+router.beforeEach((to, from, next) => {
+
+  next();
+});
+
+export default router;
